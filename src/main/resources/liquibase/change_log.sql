7910ef7acef4007553a9bfee50546d076c44fd16
Marius Coprean <cmir1645@scs.ubbcluj.ro>
2016-11-09 21:00:38 +0200

    update baza de date - marius

--- src/main/resources/liquibase/change_log.sql
+++ src/main/resources/liquibase/change_log.sql
@@ -1,49 +1,49 @@
 --liquibase formatted sql
 
 --changeset cipri:1
-CREATE TABLE user(
+CREATE TABLE users(
     id INT NOT NULL auto_increment,
-    id_rol INT,
+    id_role INT,
     name NVARCHAR(50),
     username NVARCHAR(50),
     password NVARCHAR(50),
     PRIMARY KEY(id)
     );
---rollback drop table user
+--rollback drop table users
 
 
 --changeset cipri:2
-CREATE TABLE rol
+CREATE TABLE roles
 (
     id INT NOT NULL auto_increment,
     name NVARCHAR(50),
     description NVARCHAR(50),
     PRIMARY KEY(id)
 );
---rollback drop table rol
+--rollback drop table roles
 
 --changeset cipri:3
-CREATE TABLE grup_user
+CREATE TABLE groups_users
 (
     id INT NOT NULL auto_increment,
     id_user INT,
-    id_grup INT,
+    id_group INT,
     PRIMARY KEY(id)
 );
---rollback drop table grup_user
+--rollback drop table groups_users
 
 --changeset cipri:4
-CREATE TABLE grup
+CREATE TABLE groups
 (
     id INT NOT NULL auto_increment,
     name NVARCHAR(50),
     description NVARCHAR(50),
     PRIMARY KEY(id)
 );
---rollback drop table grup
+--rollback drop table groups
 
 --changeset cipri:5
-CREATE TABLE document
+CREATE TABLE documents
 (
     id INT NOT NULL auto_increment,
     NAME NVARCHAR(50),
@@ -56,20 +56,20 @@ CREATE TABLE document
     id_template INT,
     PRIMARY KEY(id)
 );
---rollback drop table document
+--rollback drop table documents
 
 --changeset cipri:6
-CREATE TABLE document_status
+CREATE TABLE documents_statuses
 (
     id INT NOT NULL auto_increment ,
     id_doc INT,
     id_status INT,
     PRIMARY KEY(id)
 );
---rollback drop table document_status
+--rollback drop table documents_statuses
 
 --changeset cipri:7
-CREATE TABLE flux
+CREATE TABLE fluxes
 (
     id INT NOT NULL auto_increment,
     id_doc_status INT,
@@ -79,68 +79,68 @@ CREATE TABLE flux
 --rollback drop table flux
 
 --changeset cipri:8
-CREATE TABLE status
+CREATE TABLE statuses
 (
     id INT NOT NULL,
     name NVARCHAR(50),
     PRIMARY KEY(id)
 );
---rollback drop table status
+--rollback drop table statuses
 
 --changeset cipri:9
-CREATE TABLE template
+CREATE TABLE templates
 (
     id INT NOT NULL auto_increment,
     name NVARCHAR(50),
     PRIMARY KEY(id)
 );
---rollback drop table template
+--rollback drop table templates
 
 --changeset cipri:10
-ALTER TABLE user
-    ADD CONSTRAINT fk_user_rol FOREIGN KEY (id_rol)
-    REFERENCES rol(id)
+ALTER TABLE users
+    ADD CONSTRAINT fk_users_roles FOREIGN KEY (id_role)
+    REFERENCES roles(id)
 ;
---rollback ALTER TABLE user DROP FOREIGN KEY fk_user_rol
+--rollback ALTER TABLE users DROP FOREIGN KEY fk_users_roles
 
 --changeset cipri:11
-ALTER TABLE grup_user
-    ADD CONSTRAINT fk_grup_user_user FOREIGN KEY (id_user)
-    REFERENCES user(id)
+ALTER TABLE groups_users
+    ADD CONSTRAINT fk_groups_users_users FOREIGN KEY (id_user)
+    REFERENCES users(id)
 ;
---rollback ALTER TABLE grup_user DROP FOREIGN KEY fk_grup_user_user
+--rollback ALTER TABLE groups_users DROP FOREIGN KEY fk_groups_users_users
 
 --changeset cipri:12
-ALTER TABLE grup_user
-    ADD CONSTRAINT fk_grup_user_grup FOREIGN KEY (id_grup)
-    REFERENCES grup(id)
+ALTER TABLE groups_users
+    ADD CONSTRAINT fk_groups_users_groups FOREIGN KEY (id_group)
+    REFERENCES groups(id)
 ;
---rollback ALTER TABLE grup_user DROP FOREIGN KEY fk_grup_user_grup
+--rollback ALTER TABLE groups_users DROP FOREIGN KEY fk_groups_users_groups
 
 --changeset cipri:13
-ALTER TABLE document
-    ADD CONSTRAINT fk_document_user FOREIGN KEY (autor)
-    REFERENCES user(id)
+ALTER TABLE documents
+    ADD CONSTRAINT fk_documents_users FOREIGN KEY (autor)
+    REFERENCES users(id)
 ;
---rollback ALTER TABLE document DROP FOREIGN KEY fk_document_user
+--rollback ALTER TABLE documents DROP FOREIGN KEY fk_documents_users
 
 --changeset cipri:14
-ALTER TABLE document_status
-    ADD CONSTRAINT fk_document_status_document FOREIGN KEY (id_doc)
-    REFERENCES document(id)
+ALTER TABLE documents_statuses
+    ADD CONSTRAINT fk_documents_statuses_documents FOREIGN KEY (id_doc)
+    REFERENCES documents(id)
 ;
---rollback ALTER TABLE document_status DROP FOREIGN KEY fk_document_status_document
+--rollback ALTER TABLE documents_statuses DROP FOREIGN KEY fk_documents_statuses_documents
 
 --changeset cipri:16
-ALTER TABLE document_status
-    ADD CONSTRAINT fk_document_status_status FOREIGN KEY (id_status)
-    REFERENCES status(id)
+ALTER TABLE documents_statuses
+    ADD CONSTRAINT fk_documents_statuses_statuses FOREIGN KEY (id_status)
+    REFERENCES statuses(id)
 ;
---rollback ALTER TABLE document_status DROP FOREIGN KEY fk_document_status_status
+--rollback ALTER TABLE documents_statuses DROP FOREIGN KEY fk_documents_statuses_statuses
 
 --changeset cipri:17
-ALTER TABLE document
-    ADD CONSTRAINT fk_document_template FOREIGN KEY (id_template)
-    REFERENCES template(id)
+ALTER TABLE documents
+    ADD CONSTRAINT fk_documents_templates FOREIGN KEY (id_template)
+    REFERENCES templates(id)
 ;
---rollback ALTER TABLE document DROP FOREIGN KEY fk_document_template
+--rollback ALTER TABLE documents DROP FOREIGN KEY fk_documents_templates
